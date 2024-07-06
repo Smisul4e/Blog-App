95aeda66a86e1f8a5b4fd6d2cf3c50f8e99125f7
Smisul4e <teo.plamka@gmail.com>
2024-07-06 13:02:59 +0200

    Add route for creating new blog posts and update templates

--- app.py
+++ app.py
@@ -3,22 +3,18 @@ import json
 
 app = Flask(__name__)
 
-
 def load_posts():
     with open('posts.json', 'r') as file:
         return json.load(file)
-
 
 def save_posts(posts):
     with open('posts.json', 'w') as file:
         json.dump(posts, file, indent=4)
 
-
 @app.route('/')
 def index():
     posts = load_posts()
     return render_template('index.html', posts=posts)
-
 
 @app.route('/post', methods=['POST'])
 def add_post():
@@ -29,14 +25,12 @@ def add_post():
     save_posts(posts)
     return jsonify(new_post), 201
 
-
 @app.route('/post/<int:post_id>', methods=['DELETE'])
 def delete_post(post_id):
     posts = load_posts()
     posts = [post for post in posts if post['id'] != post_id]
     save_posts(posts)
     return '', 204
-
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
@@ -50,14 +44,3 @@ def add():
             'id': max(post['id'] for post in posts) + 1 if posts else 1,
             'author': author,
             'title': title,
-            'content': content
-        }
-        posts.append(new_post)
-        save_posts(posts)
-        return redirect(url_for('index'))
-
-    return render_template('add.html')
-
-
-if __name__ == '__main__':
-    app.run()
